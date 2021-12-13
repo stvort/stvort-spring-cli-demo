3860449a1a24538f85e57bd16a7f6708caade666
stvort <stvort@mail.ru>
2021-12-14 02:44:50 +0400

    CommandLineParser implementation changes to regexp

--- stvort-spring-cli-starter/src/main/java/ru/stvort/menu/CommandLineParserImpl.java
+++ stvort-spring-cli-starter/src/main/java/ru/stvort/menu/CommandLineParserImpl.java
@@ -2,54 +2,27 @@ package ru.stvort.menu;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CommandLineParserImpl implements CommandLineParser {
 
+    private static final Pattern PATTERN = Pattern.compile( "[^\\s]*\"(\\\\+\"|[^\"])*?\"|(\\\\\\s|[^\\s])+",
+            Pattern.MULTILINE | Pattern.CASE_INSENSITIVE );
+
     @Override
     public List<String> parse(String commandLine) {
-        List<String> args = new ArrayList<>();
-        do {
-            commandLine = processOneArg(commandLine, args);
-        } while (commandLine != null);
-        return args;
+        Matcher matcher = PATTERN.matcher(commandLine);
+        List<String> matches = new ArrayList<>();
+        while(matcher.find()) {
+            var arg = matcher.group();
+            if (arg.startsWith("\"") && arg.endsWith("\"")) {
+                arg = arg.substring(1, arg.length() - 1);
+            }
+            arg = arg.replaceAll("\\\\\"", "\"");
+            matches.add(arg);
+        }
+        return matches;
     }
 
-    private String processOneArg(String command, List<String> args) {
-        if (command.isEmpty()) {
-            return null;
-        }
-
-        int i = - 1;
-        int k = 1;
-        if (command.startsWith("\"")) {
-            command = command.substring(1);
-
-            i = command.indexOf('\"');
-            if (i != -1) {
-                if (i != 0 && command.charAt(i - 1) == '\\') {
-                    i = command.indexOf('\"', i + 1);
-                }
-                k = (i != -1)? 2: 1;
-            }
-            if (i == -1) {
-                i = command.indexOf(' ');
-            }
-            if (i == - 1) {
-                i = command.length();
-            }
-
-        } else {
-            i = command.indexOf(' ');
-
-            if (i == - 1) {
-                i = command.length();
-            }
-        }
-        String newArg = command.substring(0, i);
-        args.add(newArg);
-        if (newArg.length() + k > command.length()) {
-            return "";
-        }
-        return command.substring(newArg.length() + k);
-    }
 }
