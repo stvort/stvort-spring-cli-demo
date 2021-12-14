9a42aa3db3c14df77b84c5c5269a3f77342e6305
stvort <stvort@mail.ru>
2021-12-14 16:45:06 +0400

    Add CommandLineParser test && fix errors

--- stvort-spring-cli-starter/src/main/java/ru/stvort/menu/CommandLineParserImpl.java
+++ stvort-spring-cli-starter/src/main/java/ru/stvort/menu/CommandLineParserImpl.java
@@ -7,14 +7,14 @@ import java.util.regex.Pattern;
 
 public class CommandLineParserImpl implements CommandLineParser {
 
-    private static final Pattern PATTERN = Pattern.compile( "[^\\s]*\"(\\\\+\"|[^\"])*?\"|(\\\\\\s|[^\\s])+",
-            Pattern.MULTILINE | Pattern.CASE_INSENSITIVE );
+    private static final Pattern PATTERN = Pattern.compile("\"(\\\\+\"|[^\"])*?\"|(\\\\\\s|[^\\s])+",
+            Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
 
     @Override
     public List<String> parse(String commandLine) {
         Matcher matcher = PATTERN.matcher(commandLine);
         List<String> matches = new ArrayList<>();
-        while(matcher.find()) {
+        while (matcher.find()) {
             var arg = matcher.group();
             if (arg.startsWith("\"") && arg.endsWith("\"")) {
                 arg = arg.substring(1, arg.length() - 1);
@@ -24,5 +24,4 @@ public class CommandLineParserImpl implements CommandLineParser {
         }
         return matches;
     }
-
 }
